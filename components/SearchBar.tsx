9ff6902117d932d2b74741948be0726ecf522c89
Aurel Ionita <ionitaaurel32@gmail.com>
2026-09-12 19:09:32 +0300

    feat: update app configuration and enhance Index component layout

---
+++ components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SearchBar } from "react-native-screens";
+const SearchBarComponent = () => {
+  return (
+    <View>
+      <Text> </Text>
+    </View>
+  );
+};
+
+export default SearchBar;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { images } from "../../../constants/images";
 
 export default function Index() {
   return (
-    <View className="flex-1  justify-center items-center">
-      <Text className="text-5xl font-bold text-accent">Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View></View>
+      </ScrollView>
     </View>
   );
 }
